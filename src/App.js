de016cb0c5ff22d6cdb99690b60baeadf29815b3
Segun <lawalsegun72@gmail.com>
2022-10-25 13:12:29 +0100

    Add profile section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>LinkTree</h1>
+    <div className="App mt-16 flex justify-center">
+      <div className="profile-section">
+        <img
+          src={require("./assets/profile_img.jpg")}
+          alt="profile-img"
+          className="w-20 ml-3 rounded-full"
+          id="profile_id"
+        />
+        <p className="font-bold mt-2" id="twitter">
+          Segun__Lawal
+        </p>
+        <p className="font-bold mt-2 hidden" id="slack">
+          Segun
+        </p>
+      </div>
     </div>
   );
 }
